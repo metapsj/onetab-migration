be1cceb8e4ca85c675bfbbb7c57284a8ecbac465
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-16 15:41:29 -0700

    add file write to transforms folder

--- script/transform-html.rb
+++ script/transform-html.rb
@@ -7,11 +7,13 @@ LOADS_PATH = File.join(ROOT_PATH, ENV["LOADS_PATH"])
 
 # entry point
 def main
-  extracted = File.join(EXTRACTS_PATH, "export.2020-10-15.html")
-  transformed = File.join(TRANSFORMS_PATH, "export.2020-10-15.html")
+  extracted_path = File.join(EXTRACTS_PATH, "export.2020-10-15.html")
+  transformed_path = File.join(TRANSFORMS_PATH, "export.2020-10-15.html")
 
-  File.open(extracted,'r').each_with_index do |line, index|
-    puts "#{index + 1}: #{line.size}"
+  plucks = []
+
+  File.open(extracted_path,'r').each_with_index do |line, index|
+    # puts "#{index + 1}: #{line.size}"
 
     if index == 57 then
       lines = line.gsub(/\>\</, "\>\n\<").split("\n")
@@ -23,35 +25,47 @@ def main
         results << clean_element(line) if line.include?("<a class=\"clickable\" href=\"")
         results
       end
-
-      strips = []
-
-      puts
-      plucks.each{ |line| puts line }
-      puts "plucks: #{plucks.size}"
-      puts
     end
-
   end
+
 end
 
 #
 # helpers
 #
-def clean_element(line)
-  strip_class(strip_style(line))
+
+def read(file_path)
+  File.open(extracted_path,'r').each_with_index do |line, index|
+
+    if index == 57 then
+      lines = line.gsub(/\>\</, "\>\n\<").split("\n")
+
+      plucks = lines.reduce([]) do |results, line|
+        results << "" if line.include?("tabGroupTitleText")
+        results << clean_element(line) if line.include?("tabGroupTitleText")
+        results << clean_element(line) if line.include?("tabs")
+        results << clean_element(line) if line.include?("<a class=\"clickable\" href=\"")
+        results
+      end
+    end
+
+  end
 end
 
-def strip_style(line)
-  line.gsub(/\sstyle=\".*\;\"/, "")
+def transform(lines)
+  lines.reduce([]) do |results, line|
+    results << "" if line.include?("tabGroupTitleText")
+    results << clean_element(line) if line.include?("tabGroupTitleText")
+    results << clean_element(line) if line.include?("tabs")
+    results << clean_element(line) if line.include?("<a class=\"clickable\" href=\"")
+    results
+  end
 end
 
-def strip_class(line)
-  line.gsub(/\sclass=\".*\"\s/, " ")
-end
-
-def strip_id(line)
-  line.gsub(/\sid=\".*\"/, "")
+def save(file_path, plucks)
+  File.open(transformed_path,'a') do |file|
+    plucks.each{ |line| file.puts(line) }
+  end
 end
 
 #
