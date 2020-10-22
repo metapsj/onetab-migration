c18e286ef85b1838ef386a55a477db7c917f091e
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-22 12:54:21 -0700

    stub load for sqlite

---
+++ lib/loads/sqlite.rb
@@ -0,0 +1,24 @@
+#!/bin/usr/env ruby
+
+require 'sqlite3'
+
+module Loads
+  class Sqlite
+
+    def initialize
+    end
+
+    def run
+    end
+
+    private
+    
+    def read
+    end
+
+    def save
+    end
+
+  end
+end
+
