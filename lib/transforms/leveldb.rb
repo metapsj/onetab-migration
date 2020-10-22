7f40d3b6c6bfc62554e8257a47a81f6afebf2913
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-22 11:07:54 -0700

    stub a transform for leveldb

---
+++ lib/transforms/leveldb.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'leveldb'
+
+module Transforms
+  class LevelDB
+
+    def initialize
+    end
+
+    def run
+    end
+
+    private 
+
+    def read
+    end
+
+    def pluck
+    end
+
+    def cleanse
+    end
+
+    def save
+    end
+
+  end
+end
+
