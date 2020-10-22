61a89aecc06b9ced3bbbde8e2e93306653d8d039
metapsj <patrickjohnson@forty3sixteen.com>
2020-10-22 12:38:18 -0700

    stub an extract for leveldb

---
+++ lib/extracts/leveldb.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+module Extracts
+  class LevelDB
+  
+    def initialize
+    end
+
+    def run
+    end
+
+    private
+
+    def something_special
+    end
+
+  end
+end
+
